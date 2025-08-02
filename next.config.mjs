d348b76ab823d8460fa7f677ebff8bd52dae7aeb
RohitRaj766 <rohitraj2k04@gmail.com>
2025-08-02 14:07:24 +0530

    admin pannel

---
+++ next.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsHmrCache: false, // defaults to true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "udphwgckgvetdjezueaq.supabase.co",
+      },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/embed",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self'  https://your-domain.com;",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
